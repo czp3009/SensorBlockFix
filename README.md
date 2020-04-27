174e414ae65b5ea647cef1d25afc5d2078902be7
czp3009 <czp3009@gmail.com>
2020-04-28 00:08:37 +0800

    deprecated

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-﻿# SensorBlockFix
+﻿# Deprecated
+This plugin is deprecated, please use another plugin [here](https://github.com/czp3009/DataValidateFix)
+
+# SensorBlockFix
 A torch plugin that fix MySensorBlock data validate issue.
 
 If player use modified client to send a very big value of LeftExtend, RightExtend or other options in MySensorBlock UI. Then call `IMySensorBlock.DetectedEntities(List)` method in ProgramBlock, player will get as many grids' position as he want.
--- SensorBlockFix/SensorBlockFixPlugin.cs
+++ SensorBlockFix/SensorBlockFixPlugin.cs
@@ -1,8 +1,43 @@
-﻿using Torch;
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Navigation;
+using Torch;
+using Torch.API.Plugins;
 
 namespace SensorBlockFix
 {
-    public class SensorBlockFixPlugin : TorchPluginBase
+    public class SensorBlockFixPlugin : TorchPluginBase, IWpfPlugin
     {
+        private readonly Lazy<UserControl> _userControl = new Lazy<UserControl>(InitUserControl);
+
+        private static UserControl InitUserControl()
+        {
+            var hyperLink = new Hyperlink
+            {
+                Inlines = {"Here"},
+                NavigateUri = new Uri("https://torchapi.net/plugins/item/d4948c7d-5363-469c-a24a-48d64ed02073")
+            };
+            hyperLink.RequestNavigate += (sender, e) =>
+            {
+                Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
+                e.Handled = true;
+            };
+
+            return new UserControl
+            {
+                Content = new TextBlock
+                {
+                    Inlines =
+                    {
+                        "This plugin is deprecated, please use another plugin ",
+                        hyperLink
+                    }
+                }
+            };
+        }
+
+        public UserControl GetControl() => _userControl.Value;
     }
 }
